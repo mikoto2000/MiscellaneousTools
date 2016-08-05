a12fc29c54c4252cc88c492ebfd9d2bd9d8d5f54
mikoto2000 <mikoto2000@gmail.com>
2016-08-06 02:26:54 +0900

    Updated.
    - tts.bat の出力ファイル名をいい感じにするように修正
    - toMp3.bat の出力ファイル名をいい感じにするように修正
    
    いい感じ : 拡張子を単純に追加するのをやめて、
               元の拡張子を削除したうえで追加するようにした

--- win/toMp3.bat
+++ win/toMp3.bat
@@ -3,12 +3,12 @@
 rem toMp3.bat
 rem
 rem ffmpeg を使用して、引数で受け取ったファイルを mp3 に変換する。
-rem 変換後ファイル名は変換前ファイル名に「.mp3」を追加したものになる。
+rem 変換後ファイル名は変換前ファイル名の拡張子を「.mp3」に変更したものになる。
 rem 変換後ファイルは変換前ファイルと同じディレクトリに作成される。
 rem
 rem 引数 : ffmpeg で変換可能な音声ファイルのリスト
 
 for %%f in (%*) do (
-    start cmd /c ffmpeg -i %%f %%f.mp3
+    start cmd /c ffmpeg -i %%f %%~dpnf.mp3
 )
 
--- win/tts.bat
+++ win/tts.bat
@@ -2,13 +2,13 @@
 rem tts.bat
 rem
 rem VoiceroidController を使用して、引数で受け取ったテキストファイルを wav に変換する。
-rem 変換後ファイル名は変換前ファイル名に「.wav」を追加したものになる。
+rem 変換後ファイル名は変換前ファイル名の拡張子を「.wav」に変更したものになる。
 rem 変換後ファイルは変換前ファイルと同じディレクトリに作成される。
 rem
 rem 引数 : UTF-8 で記述されたテキストファイルのリスト
 
 for %%f in (%*) do (
-    VoiceroidController --split-size 20000 -u -s -i "%%~ff" -o "%%~ff.wav"
+    VoiceroidController --split-size 20000 -u -s -i "%%~ff" -o "%%~dpnf.wav"
 )
 
 pause
