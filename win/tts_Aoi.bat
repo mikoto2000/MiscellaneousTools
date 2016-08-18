de6f28e3249a090b9e84d321070af53d37fe3db8
mikoto2000 <mikoto2000@gmail.com>
2016-08-18 23:03:33 +0900

    Added 'tts_Akane.bat', 'tts_Aoi.bat'.

---
+++ win/tts_Aoi.bat
@@ -0,0 +1,15 @@
+
+rem tts_Aoi.bat
+rem
+rem VoiceroidController を使用して、引数で受け取ったテキストファイルを wav に変換する。
+rem 使用 VOICEROID は「琴葉葵」。
+rem 変換後ファイル名は変換前ファイル名の拡張子を「.wav」に変更したものになる。
+rem 変換後ファイルは変換前ファイルと同じディレクトリに作成される。
+rem
+rem 引数 : UTF-8 で記述されたテキストファイルのリスト
+
+for %%f in (%*) do (
+    VoiceroidController --voiceroid Aoi --split-size 20000 -u -s -i "%%~ff" -o "%%~dpnf.wav"
+)
+
+pause
