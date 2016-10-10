cd7e067d3bca125424b170a83f491ae8ed164fc2
mikoto2000 <mikoto2000@gmail.com>
2016-10-11 00:01:44 +0900

    Modified encoding 'toMp3.bat', utf8 to sjis.

--- win/toMp3.bat
+++ win/toMp3.bat
@@ -2,11 +2,11 @@
 
 rem toMp3.bat
 rem
-rem ffmpeg を使用して、引数で受け取ったファイルを mp3 に変換する。
-rem 変換後ファイル名は変換前ファイル名の拡張子を「.mp3」に変更したものになる。
-rem 変換後ファイルは変換前ファイルと同じディレクトリに作成される。
+rem ffmpeg ���g�p���āA�����Ŏ󂯎�����t�@�C���� mp3 �ɕϊ�����B
+rem �ϊ���t�@�C�����͕ϊ��O�t�@�C�����̊g���q���u.mp3�v�ɕύX�������̂ɂȂ�B
+rem �ϊ���t�@�C���͕ϊ��O�t�@�C���Ɠ����f�B���N�g���ɍ쐬�����B
 rem
-rem 引数 : ffmpeg で変換可能な音声ファイルのリスト
+rem ���� : ffmpeg �ŕϊ��\�ȉ����t�@�C���̃��X�g
 
 for %%f in (%*) do (
     start cmd /c ffmpeg -i %%f %%~dpnf.mp3
