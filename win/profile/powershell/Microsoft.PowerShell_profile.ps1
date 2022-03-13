27f1f737629683dd603417672bcc276aa540a4a9
mikoto2000 <mikoto2000@gmail.com>
2022-03-13 12:55:54 +0900

    docker run コマンドの省略形を PowerShell プロファイルに追加。

--- win/profile/powershell/Microsoft.PowerShell_profile.ps1
+++ win/profile/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,13 @@
 Set-Alias dc docker-compose
 
+function dr() {
+    docker run -it --rm $args
+}
+
+function drv() {
+    docker run -it --rm -v "$(pwd):/work" --workdir="/work" $args
+}
+
 # ColorTool がインストール済みならそれを使ってコンソールの色設定を行う
 if (Get-Command('ColorTool.exe') -ErrorAction SilentlyContinue) {
     ColorTool.exe -q OneHalfDark
