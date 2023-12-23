472ceb478a5826ff551e4848ce205bc0222eb121
mikoto2000 <mikoto2000@gmail.com>
2023-12-24 05:24:30 +0900

    Updated `vim-openparent-for-wsl.sh`.

--- vim/vim-openparent-for-wsl.sh
+++ vim/vim-openparent-for-wsl.sh
@@ -3,25 +3,10 @@
 # vim-openparent-for-wsl.sh
 #
 # Usage:
-#     vim-openparent-for-wsl.sh FILE_PATH [VIM_SERVER_NAME]
+#     vim-openparent-for-wsl.sh
 #
 # WSL のターミナルから、ホストの vim server でファイルを開くためのスクリプト。
 #
-if [ $# -lt 1 ];then
-    echo "Usage: $0 FILE_PATH [VIM_SERVER_NAME]";
-    exit 1
-fi
-
-if [ "$2" == "" ];then
-  VIM_SERVER=$(vim.exe --serverlist | head -n 1 | sed -e "s/\r//g")
-else
-  VIM_SERVER=$2
-fi
-
-if [ "$VIM_SERVER" == "" ];then
-  gvim.exe $1
-else
-  gvim.exe --remote-send "<Cmd>split<CR>"
-  gvim.exe --remote "$1"
-fi
+gvim.exe --remote-send "<Cmd>split<CR>"
+gvim.exe --remote "$1"
 
